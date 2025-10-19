d61ffc03b2680fbab042224ef9f63fc506f05270
uneasywatermelon <marioalemany16@gmail.com>
2025-10-18 21:09:00 -0700

    learned something about lambdas

--- creating_strings.cpp
+++ creating_strings.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int factorial(int n) {
-    int ret = 1;
-    for(int x=2;x<=n;++x)ret *= x;
-    return ret;
-}
+
 int main() {
-
     string s;
-    cin >> s;
-    unordered_map<char,int>dt;
-    for (char c:s) {
-        dt[c]++;
+    cin>>s;
+    int t=s.size();
+    int f;
+    auto fact = [&](int x) -> int {
+        f=1;
+        for(int r=2;r<=x;++r)f*=r;
+        return f;
+    };
+    t=fact(t);
+    int a[26]={0};
+    for(char c:s)a[c-'a']++;
+    for(int x:a)t/=fact(x);
+    cout << t << endl;
+    
+    for(;t--;){
+        cout << s << '\n';
+        next_permutation(s.begin(),s.end());
     }
 
-    int t = 0;
-    t = factorial(s.size());
-    for (pair x: dt) t/=factorial(x.second);
-    cout << t << endl;
-    while(t--){
-        
-        cout << s << '\n';
-        next_permutation(s.begin(), s.end());
-    }
 }
