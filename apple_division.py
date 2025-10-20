a21870be47dba7e02524d776d374bcd5789a41fb
uneasywatermelon <uneasywatermelon@users.noreply.github.com>
2025-10-20 09:53:03 -0700

    apple division

--- apple_division.py
+++ apple_division.py
@@ -1,1 +1,24 @@
+input()
+nums = list(map(int,input().split()))
+ans = []
+curr = []
+def dfs(i):
+    if i >= len(nums):
+        ans.append(curr.copy())
+        return
 
+    curr.append(nums[i])
+    dfs(i + 1)
+
+    curr.pop()
+    dfs(i + 1)
+
+dfs(0)
+
+tot = sum(nums)
+sms = [sum(a) for a in ans]
+mn = float('inf')
+for x in sms:
+    a = (abs(2*x - tot))
+    mn = min(a,mn)
+print(mn)
